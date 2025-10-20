a12020db631a614098a692de573cd145e495258c
marcos <marcoslopez1305@gmail.com>
2025-10-20 20:48:56 +0200

    update

--- cubed.h
+++ cubed.h
@@ -4,24 +4,27 @@ typedef struct s_game
 	char	*so_texture;
 	char	*ea_texture;
 	char	*we_texture;
+
 	int		floor_color;
 	int		ceiling_color;
-	double	player_x;
-	double	player_y;
+
 	char	**map;
 	int		width;
 	int		height;
 
+	double	player_x;
+	double	player_y;
+	double	angle;
+
+	int		right;
+	int		left;
+	int		up;
+	int		down;
+
+	double	delta_x;
+	double	delta_y;
+
 	void	*mlx;
 	void	*win;
-
-	double	dir_x;
-	double	dir_y;
-
-	double	plane_x;
-	double	plane_y;
-
-	double	time;
-	double	old_time;
 } t_game;
 
--- main.c
+++ main.c
@@ -2,9 +2,14 @@
 #include "cubed.h"
 #include "libft/libft.h"
 #include <stdlib.h>
+#include <math.h>
 
-#define WINDOW_WIDTH 640
-#define WINDOW_HEIGHT 480
+#define WIN_WIDTH 640
+#define WIN_HEIGHT 480
+
+#define FOV 1.05
+
+#define PI 3.14159265359
 
 char	**init_map()
 {
@@ -30,15 +35,10 @@ t_game	*init_game()
 	g = (t_game *)malloc(sizeof(t_game));
 	g->player_x = 3;
 	g->player_y = 3;
-	g->dir_x = 0;
-	g->dir_y = -1;
-	g->plane_x = 0.66;
-	g->plane_y = 0;
-	g->time = 0;
-	g->old_time = 0;
 	g->map = init_map();
+	g->angle = PI / 2;
 	g->mlx = mlx_init();
-	g->win = mlx_new_window(g->mlx, WINDOW_WIDTH, WINDOW_HEIGHT,  "cubed");
+	g->win = mlx_new_window(g->mlx, WIN_WIDTH, WIN_HEIGHT,  "cubed");
 	return (g);
 }
 
@@ -47,5 +47,32 @@ int main(void)
 	t_game	*g;
 
 	g = init_game();
+
+	int i = 0;
+	double ray_ang = g->angle - (FOV / 2);
+	double step_ang = FOV / WIN_WIDTH;
+	while (i < WIN_WIDTH)
+	{
+		g->up = ray_ang < PI && ray_ang > 0;
+		g->down = !g->up;
+		g->left = ray_ang > (PI / 2) && ray_ang < (3 * PI / 2);
+		g->right = !g->left;
+
+		if (g->right)
+			g->delta_x = round(g->player_x) - g->player_x + 0.5;
+		else
+			g->delta_x = round(g->player_x) + g->player_x + 0.5;
+
+		if (g->up)
+			g->delta_y = round(g->player_y) + g->player_y + 0.5;
+		else
+			g->delta_y = round(g->player_y) - g->player_y + 0.5;
+
+		// TODO: working here
+
+		ray_ang += step_ang;
+		i++;
+	}
+
 	mlx_loop(g->mlx);
 }
